4e99ade7534c483301674a14e62605f4423c688c
anzhela.sukhanova <anzhela.sukhanova@jetbrains.com>
2023-12-27 14:33:31 +0200

    Test for ^KT-61937
    Add the tests generated

--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated.java
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated.java
@@ -13474,6 +13474,12 @@ public class LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated
                 }
 
                 @Test
+                @TestMetadata("functionContext.kt")
+                public void testFunctionContext() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/extensions/contextReceivers/functionContext.kt");
+                }
+
+                @Test
                 @TestMetadata("functionalType.kt")
                 public void testFunctionalType() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/extensions/contextReceivers/functionalType.kt");
--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirPsiOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirPsiOldFrontendDiagnosticsTestGenerated.java
@@ -13474,6 +13474,12 @@ public class FirPsiOldFrontendDiagnosticsTestGenerated extends AbstractFirPsiDia
                 }
 
                 @Test
+                @TestMetadata("functionContext.kt")
+                public void testFunctionContext() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/extensions/contextReceivers/functionContext.kt");
+                }
+
+                @Test
                 @TestMetadata("functionalType.kt")
                 public void testFunctionalType() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/extensions/contextReceivers/functionalType.kt");
