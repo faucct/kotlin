08fabfa84dbb3fe573253213a8f23a4dad54e1fc
Leonid Startsev <leonid.startsev@jetbrains.com>
2024-01-11 16:03:25 +0100

    fixup read utils

--- libraries/kotlinx-metadata/src/kotlin/metadata/internal/ReadUtils.kt
+++ libraries/kotlinx-metadata/src/kotlin/metadata/internal/ReadUtils.kt
@@ -13,8 +13,8 @@ import org.jetbrains.kotlin.metadata.ProtoBuf.Annotation.Argument.Value.Type.*
 import org.jetbrains.kotlin.metadata.deserialization.Flags
 import org.jetbrains.kotlin.metadata.deserialization.NameResolver
 
-public fun ProtoBuf.Annotation.readAnnotation(strings: NameResolver): kotlin.metadata.KmAnnotation =
-    kotlin.metadata.KmAnnotation(
+public fun ProtoBuf.Annotation.readAnnotation(strings: NameResolver): KmAnnotation =
+    KmAnnotation(
         strings.getClassName(id),
         argumentList.mapNotNull { argument ->
             argument.value.readAnnotationArgument(strings)?.let { value ->
@@ -23,36 +23,36 @@ public fun ProtoBuf.Annotation.readAnnotation(strings: NameResolver): kotlin.met
         }.toMap()
     )
 
-public fun ProtoBuf.Annotation.Argument.Value.readAnnotationArgument(strings: NameResolver): kotlin.metadata.KmAnnotationArgument? {
+public fun ProtoBuf.Annotation.Argument.Value.readAnnotationArgument(strings: NameResolver): KmAnnotationArgument? {
     if (Flags.IS_UNSIGNED[flags]) {
         return when (type) {
-            BYTE -> kotlin.metadata.KmAnnotationArgument.UByteValue(intValue.toByte().toUByte())
-            SHORT -> kotlin.metadata.KmAnnotationArgument.UShortValue(intValue.toShort().toUShort())
-            INT -> kotlin.metadata.KmAnnotationArgument.UIntValue(intValue.toInt().toUInt())
-            LONG -> kotlin.metadata.KmAnnotationArgument.ULongValue(intValue.toULong())
+            BYTE -> KmAnnotationArgument.UByteValue(intValue.toByte().toUByte())
+            SHORT -> KmAnnotationArgument.UShortValue(intValue.toShort().toUShort())
+            INT -> KmAnnotationArgument.UIntValue(intValue.toInt().toUInt())
+            LONG -> KmAnnotationArgument.ULongValue(intValue.toULong())
             else -> error("Cannot read value of unsigned type: $type")
         }
     }
 
     return when (type) {
-        BYTE -> kotlin.metadata.KmAnnotationArgument.ByteValue(intValue.toByte())
-        CHAR -> kotlin.metadata.KmAnnotationArgument.CharValue(intValue.toInt().toChar())
-        SHORT -> kotlin.metadata.KmAnnotationArgument.ShortValue(intValue.toShort())
-        INT -> kotlin.metadata.KmAnnotationArgument.IntValue(intValue.toInt())
-        LONG -> kotlin.metadata.KmAnnotationArgument.LongValue(intValue)
-        FLOAT -> kotlin.metadata.KmAnnotationArgument.FloatValue(floatValue)
-        DOUBLE -> kotlin.metadata.KmAnnotationArgument.DoubleValue(doubleValue)
-        BOOLEAN -> kotlin.metadata.KmAnnotationArgument.BooleanValue(intValue != 0L)
-        STRING -> kotlin.metadata.KmAnnotationArgument.StringValue(strings.getString(stringValue))
+        BYTE -> KmAnnotationArgument.ByteValue(intValue.toByte())
+        CHAR -> KmAnnotationArgument.CharValue(intValue.toInt().toChar())
+        SHORT -> KmAnnotationArgument.ShortValue(intValue.toShort())
+        INT -> KmAnnotationArgument.IntValue(intValue.toInt())
+        LONG -> KmAnnotationArgument.LongValue(intValue)
+        FLOAT -> KmAnnotationArgument.FloatValue(floatValue)
+        DOUBLE -> KmAnnotationArgument.DoubleValue(doubleValue)
+        BOOLEAN -> KmAnnotationArgument.BooleanValue(intValue != 0L)
+        STRING -> KmAnnotationArgument.StringValue(strings.getString(stringValue))
         CLASS -> strings.getClassName(classId).let { className ->
             if (arrayDimensionCount == 0)
-                kotlin.metadata.KmAnnotationArgument.KClassValue(className)
+                KmAnnotationArgument.KClassValue(className)
             else
-                kotlin.metadata.KmAnnotationArgument.ArrayKClassValue(className, arrayDimensionCount)
+                KmAnnotationArgument.ArrayKClassValue(className, arrayDimensionCount)
         }
-        ENUM -> kotlin.metadata.KmAnnotationArgument.EnumValue(strings.getClassName(classId), strings.getString(enumValueId))
-        ANNOTATION -> kotlin.metadata.KmAnnotationArgument.AnnotationValue(annotation.readAnnotation(strings))
-        ARRAY -> kotlin.metadata.KmAnnotationArgument.ArrayValue(arrayElementList.mapNotNull { it.readAnnotationArgument(strings) })
+        ENUM -> KmAnnotationArgument.EnumValue(strings.getClassName(classId), strings.getString(enumValueId))
+        ANNOTATION -> KmAnnotationArgument.AnnotationValue(annotation.readAnnotation(strings))
+        ARRAY -> KmAnnotationArgument.ArrayValue(arrayElementList.mapNotNull { it.readAnnotationArgument(strings) })
         null -> null
     }
 }
