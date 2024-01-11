31a8087e7ab33da1890cc8ec3ef18a2262c4a262
strangepleasures <pavel.mikhailovskii@jetbrains.com>
2024-01-11 17:57:01 +0100

    [KAPT] Support type annotations in K2 KAPT. Fixes KT-64479

--- plugins/kapt4/src/org/jetbrains/kotlin/kapt4/StubGenerator.kt
+++ plugins/kapt4/src/org/jetbrains/kotlin/kapt4/StubGenerator.kt
@@ -430,14 +430,11 @@ private class StubGenerator(
 
             private fun Printer.printType(type: PsiType) {
                 recordErrorTypes(type)
-                type.annotations.forEach {
-                    printAnnotation(it, false)
-                }
-                printTypeSignature(type)
+                printTypeSignature(type, true)
             }
 
-            private fun Printer.printTypeSignature(type: PsiType) {
-                printWithNoIndent((if (type is PsiClassType && isErroneous(type)) type.rawType() else type).canonicalText.replace('$', '.'))
+            private fun Printer.printTypeSignature(type: PsiType, annotated: Boolean = false) {
+                printWithNoIndent((if (type is PsiClassType && isErroneous(type)) type.rawType() else type).getCanonicalText(annotated).replace('$', '.'))
             }
 
             private fun Printer.printTypeParams(typeParameters: Array<PsiTypeParameter>) {
