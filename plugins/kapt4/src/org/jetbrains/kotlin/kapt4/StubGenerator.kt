893154e16fa9def2c3f2b6e54fd9d30b038a9094
strangepleasures <pavel.mikhailovskii@jetbrains.com>
2024-01-11 17:59:41 +0100

    A nano refactoring

--- plugins/kapt4/src/org/jetbrains/kotlin/kapt4/StubGenerator.kt
+++ plugins/kapt4/src/org/jetbrains/kotlin/kapt4/StubGenerator.kt
@@ -392,7 +392,7 @@ private class StubGenerator(
                 print(method.name, "(")
                 method.parameterList.parameters.forEachIndexed{ index, parameter ->
                     if (index > 0) print(", ")
-                    printTypeSignature(parameter.type)
+                    printTypeSignature(parameter.type, false)
                 }
                 print(")")
             }
@@ -433,7 +433,7 @@ private class StubGenerator(
                 printTypeSignature(type, true)
             }
 
-            private fun Printer.printTypeSignature(type: PsiType, annotated: Boolean = false) {
+            private fun Printer.printTypeSignature(type: PsiType, annotated: Boolean) {
                 printWithNoIndent((if (type is PsiClassType && isErroneous(type)) type.rawType() else type).getCanonicalText(annotated).replace('$', '.'))
             }
 
