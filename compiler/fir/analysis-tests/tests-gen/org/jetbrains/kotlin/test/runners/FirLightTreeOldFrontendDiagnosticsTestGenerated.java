fd58a01b9019770ebf617dc78904748d043e0ef6
Anastasia.Nekrasova <anastasia.nekrasova@jetbrains.com>
2024-01-12 15:51:18 +0200

    [K2]: Support java-kotlin interop for @SubclassOptInRequired
    Previously, if the parent class was in Java code and the subclass in
    Kotlin, the opt-in usage error would not be reported for the subclass.
    The problem was that the extractClassFromArgument function couldn't get
    the class type from Java code. It was looking for a ConeClassLikeType,
    but found a ConeFlexibleType instead.
    
    ^KT-60258

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
@@ -39950,6 +39950,12 @@ public class FirLightTreeOldFrontendDiagnosticsTestGenerated extends AbstractFir
                 }
 
                 @Test
+                @TestMetadata("JavaKotlinInterop.kt")
+                public void testJavaKotlinInterop() throws Exception {
+                    runTest("compiler/testData/diagnostics/testsWithStdLib/annotations/subclassOptInRequired/JavaKotlinInterop.kt");
+                }
+
+                @Test
                 @TestMetadata("NestedClasses.kt")
                 public void testNestedClasses() throws Exception {
                     runTest("compiler/testData/diagnostics/testsWithStdLib/annotations/subclassOptInRequired/NestedClasses.kt");
